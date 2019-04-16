2a39f40ebf27911767997bed726bc440dbe34900
Pawel Soltysiak <psoltysiak@nvidia.com>
2019-04-16 11:23:43 +0200

    Added patched version of slim's resnet_v1 allowing to get pre-fc layer end point

--- model/__init__.py
+++ model/__init__.py
@@ -1,1 +1,2 @@
 from model import fcn
+from model import resnet_v1
--- model/fcn.py
+++ model/fcn.py
@@ -8,8 +8,7 @@ import horovod.tensorflow as hvd
 
 from utils.optimizers import FixedLossScalerOptimizer
 
-import tensorflow.contrib.slim as slim
-from tensorflow.contrib.slim.nets import resnet_v1
+from model import resnet_v1
 
 from dllogger.logger import LOGGER
 
@@ -30,7 +29,7 @@ class FCN(object):
        
         with tf.variable_scope("rgb_branch"):
             with tf.contrib.slim.arg_scope(resnet_v1.resnet_arg_scope()):
-                rgb_logits, end_points = resnet_v1.resnet_v1_101(features[0], self.num_classes, is_training=is_training)        
+                rgb_logits, rgb_end_points = resnet_v1.resnet_v1_101(features[0], self.num_classes, is_training=is_training)        
 
                 #Exclude logits
                 rgb_variables_to_restore = tf.contrib.slim.get_variables_to_restore(exclude=['rgb_branch/resnet_v1_101/logits'])
@@ -44,7 +43,7 @@ class FCN(object):
         
         with tf.variable_scope("depth_branch"):
             with tf.contrib.slim.arg_scope(resnet_v1.resnet_arg_scope()):     
-                depth_logits, end_points = resnet_v1.resnet_v1_101(features[1], self.num_classes, is_training=is_training)
+                depth_logits, depth_end_points = resnet_v1.resnet_v1_101(features[1], self.num_classes, is_training=is_training)
 
                 #Exclude rgb branch already existing in the graph and logits
                 depth_variables_to_restore = tf.contrib.slim.get_variables_to_restore(exclude=['rgb_branch', 'depth_branch/resnet_v1_101/logits'])
@@ -57,75 +56,41 @@ class FCN(object):
       
                 tf.train.init_from_checkpoint(self.pre_trained_model_path, depth_assignment_map)
 
-        rgb_branch_last_conv = tf.contrib.framework.get_variables('rgb_branch/resnet_v1_101/block4/unit_3/bottleneck_v1/conv3/weights')[0]
-        depth_branch_last_conv = tf.contrib.framework.get_variables('depth_branch/resnet_v1_101/block4/unit_3/bottleneck_v1/conv3/weights')[0]
-            
-        print(rgb_branch_last_conv.get_shape())
-        print(depth_branch_last_conv.get_shape())  
+        rgb_branch_output = rgb_end_points['pre_fully_connected']
+        depth_branch_output = depth_end_points['pre_fully_connected']
         
-        model = tf.concat( [rgb_branch_last_conv, depth_branch_last_conv], axis=2, name='branches_concat')
+        print(rgb_branch_output)
+        print(depth_branch_output)
+        
+        model = tf.concat( [rgb_branch_output, depth_branch_output], axis=2, name='branches_concat')
         print(model)
-        model = slim.conv2d(model, 512, [1, 1], scope='conv1_1')
+        model = tf.contrib.slim.conv2d(model, 512, [1, 1], scope='conv1_1')
         print(model)
-        model = slim.conv2d(model, 128, [1, 1], scope='conv2_1')
+        model = tf.contrib.slim.conv2d(model, 128, [1, 1], scope='conv2_1')
         print(model)
-        model = slim.conv2d(model, 3, [1, 1], scope='conv3_1')
-        print(model)
+        logits = tf.contrib.slim.conv2d(model, 3, [1, 1], scope='logits')
+        print(logits)
+        print(labels)
+        # Use Spatial Up Sampling Bilinear
         
-        # it's height, width in TF - not width, height
-        #new_height = int(round(1.0 * 2.0))
-        #new_width = int(round(1.0 * 2.0))
-        #model = tf.image.resize_images(input_tensor, [model, new_width])
         
-        '''
-            predictions = {
-              'classes': tf.argmax(input=logits, axis=1),
-              'probabilities': tf.nn.softmax(logits, name='softmax_tensor')
-            }
+        # USE Symmetric Padding
         
-            if mode == tf.estimator.ModeKeys.PREDICT:
-                return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+        # 
+        # Convert end_points_collection into a dictionary of end_points.
         
-            accuracy = tf.metrics.accuracy(labels=labels, predictions=predictions["classes"])
-            tf.summary.scalar('accuracy', accuracy[1])
+        loss = tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=labels)  
+        #reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+        #total_loss = tf.add_n([loss] + reg_losses, name='total_loss')
+                
+        global_step = tf.train.get_or_create_global_step()
+        optimizer = tf.train.MomentumOptimizer(learning_rate=0.0001, momentum=0.99)
+        train_op = optimizer.minimize(loss, global_step)
         
-            # Restore all the variables except from the last layer excluding fully connected layers and removing batch norm
-
-
-            variables_to_restore = tf.contrib.slim.get_variables_to_restore(exclude=['vgg_16/fc8'])
-            for v in variables_to_restore:
-                print(v)
-            #print(variables_to_restore)
-            scopes = { os.path.dirname(v.name) for v in variables_to_restore }
-            tf.train.init_from_checkpoint(pre_trained_model_path, 
-                                  {v.name.split(':')[0]: v for v in variables_to_restore})
-
-
-            # Get a handle to last variable and initalize it from scratch
-            fc8_variables = tf.contrib.framework.get_variables('vgg_16/fc8')
-            fc8_init = tf.variables_initializer(fc8_variables)
-
-            loss = tf.losses.sparse_softmax_cross_entropy(logits=logits, labels=labels)  
-            reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
-            total_loss = tf.add_n([loss] + reg_losses, name='total_loss')   
-
-            if mode == tf.estimator.ModeKeys.EVAL:
-                metrics = {'eval_accuracy': accuracy}
-                return tf.estimator.EstimatorSpec(
-                    mode, loss=loss, eval_metric_ops=metrics)
-
-            # Re-train the last layer of model
-            global_step = tf.train.get_or_create_global_step()
-            optimizer = tf.train.AdamOptimizer(learning_rate=0.00016)
-            train_op = optimizer.minimize(total_loss, global_step, var_list=fc8_variables)
-
-            logging_hook = tf.train.LoggingTensorHook({"loss" : loss, 'total_loss' : total_loss}, every_n_iter=10)
-
-            return tf.estimator.EstimatorSpec(
-                mode=mode,
-                predictions=predictions,
-                loss=total_loss,
-                train_op=train_op,
-                training_hooks = [logging_hook])
-
-        '''
+        logging_hook = tf.train.LoggingTensorHook({"loss" : loss} , every_n_iter=10)
+     
+        return tf.estimator.EstimatorSpec(
+            mode=mode,
+            loss=loss,
+            train_op=train_op,
+            training_hooks = [logging_hook])
